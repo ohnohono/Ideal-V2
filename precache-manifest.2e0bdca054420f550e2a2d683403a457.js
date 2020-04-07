d7d688924b20811d043d145839c30a0559e2da29
ohnohono <business@alecacosta.me>
2020-04-07 11:58:47 -0700

    Updates

---
+++ precache-manifest.2e0bdca054420f550e2a2d683403a457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56adde84d6e01e534ec47624244b6f0c",
+    "url": "/Ideal-V2/index.html"
+  },
+  {
+    "revision": "f07b2187054ff0b7ae6c",
+    "url": "/Ideal-V2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4161ada704afbf4a3bf6",
+    "url": "/Ideal-V2/static/css/main.bcaac245.chunk.css"
+  },
+  {
+    "revision": "f07b2187054ff0b7ae6c",
+    "url": "/Ideal-V2/static/js/2.53a16b42.chunk.js"
+  },
+  {
+    "revision": "f03fb4cbfdf17f599208090bbf9a01ef",
+    "url": "/Ideal-V2/static/js/2.53a16b42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4161ada704afbf4a3bf6",
+    "url": "/Ideal-V2/static/js/main.e4b1b742.chunk.js"
+  },
+  {
+    "revision": "fe225a15f9669f11fb95",
+    "url": "/Ideal-V2/static/js/runtime-main.0f405ede.js"
+  }
+]);
